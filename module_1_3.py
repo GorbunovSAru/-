d1f1c36266dcec1792826840805c7be1a54080ed
GorbunovSAru <gorbunovsa@gmail.com>
2024-10-16 20:20:31 +0800

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,15 @@
+# module_1_3.py
+from operator import truediv
+
+name = "Сергей"
+print("Name:" , name)
+new = 5
+age = 56
+print("Age:", age)
+
+
+NewAge =(age + new)
+print("NewAge:", NewAge)
+
+is_student = True
+print("is_student:",is_student)
